da3799ef76c9e0c3497689890351b4a4e7556054
fisnikbatusha <batusha.fisnik@gmail.com>
2024-01-23 19:19:53 +0100

    Fixed tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,7 +5,7 @@ const homeURL = Cypress.config().baseUrl;
 describe('Testimi i Loginit', () => {
   it('Testimi i Loginit', () => {
     cy.visit(homeURL);
-    cy.get('a').contains('Sign Ins').click();
+    cy.get('a').contains('Sign In').click();
     cy.get(loginPagePoPet.usernameInputField).clear().type(loginPagePoPet.username);
     cy.get(loginPagePoPet.passwordInputField).clear().type(loginPagePoPet.loginPassword);
     cy.get(loginPagePoPet.loginButton).click();
